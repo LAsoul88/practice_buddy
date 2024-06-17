c8bf698d31d1255734e5f5c3a340ceea67c60e0a
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-17 12:19:39 -0700

    added pause svg content, tracking if metronome is running, conditionally rendering pause/play button for metronome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,45 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { Button } from '@/app/(component)/Button'
+import { Icon } from '@/app/(component)/Icon'
 import Metronome from '@/util/metronome'
 
 export default function Home() {
-  const [metronome, setMetronome] = useState<Metronome>()
+  const [metronome, setMetronome] = useState<Metronome>({} as Metronome)
+  const [isRunning, setIsRunning] = useState(false)
 
   const startMetronome = () => {
-    if (metronome) metronome.startStop()
+    if (metronome) {
+      metronome.startStop()
+      setIsRunning(!isRunning)
+    }
     console.log(metronome)
   }
 
-  const createMetronome = () => {
+  useEffect(() => {
     setMetronome(new Metronome)
-  }
+  }, [])
 
-  console.log(metronome)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to Practice Buddy</h1>
       <Button 
         onClick={() => {
-          createMetronome()
-        }}
-      >
-        Create
-      </Button>
-      <Button 
-        onClick={() => {
           startMetronome()
         }}
       >
-        Start
+        { isRunning 
+            ? 
+          <Icon 
+            path={'/images/pause.svg'} 
+            alt={'pause button'} 
+          />
+            :
+          <Icon 
+            path={'/images/play.svg'} 
+            alt={'play button'} 
+          />
+        }
       </Button>
     </main>
   )
