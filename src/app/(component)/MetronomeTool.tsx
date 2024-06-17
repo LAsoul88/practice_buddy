6f6dcbb5be994955afb2c983f70dfcd8b8817ed3
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-17 09:42:48 -0700

    implementing metronome template

---
+++ src/app/(component)/MetronomeTool.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+
+const MetronomeTool = () => {
+  const [audioContext, setAudioContext] = useState<AudioContext>()
+  return (
+    <div></div>
+  )
+}
+
+export default MetronomeTool
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { Button } from '@/app/(component)/Button'
+import Metronome from '@/util/metronome'
 
 export default function Home() {
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null)
+  const [metronome, setMetronome] = useState<Metronome>()
+
+  const startMetronome = () => {
+    if (metronome) metronome.startStop()
+    console.log(metronome)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const val = window.AudioContext = window.AudioContext || window.webkitAudioContext
+      setAudioContext(new val)
+      if (audioContext !== null) {
+        setMetronome(new Metronome(audioContext))
+      }
+    }
+  }, [])
+  console.log(metronome)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to Practice Buddy</h1>
-      
+      <Button 
+        onClick={() => {
+          startMetronome()
+        }}
+      >
+        Start
+      </Button>
     </main>
-  );
+  )
 }
