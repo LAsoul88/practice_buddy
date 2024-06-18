745699a76d8ea2d2dfb77fcd96e02b0960ffc56f
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-17 17:25:51 -0700

    moved metronome logic into it's own component and imported into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,11 @@
-'use client'
-import { useState } from 'react'
-import { Button } from '@/app/(component)/Button'
-import { Icon } from '@/app/(component)/Icon'
-import Metronome from '@/util/metronome'
-
+import { MetronomeTool } from '@/app/(component)/MetronomeTool'
 export default function Home() {
-  const [metronome, setMetronome] = useState<Metronome>()
-  const [isRunning, setIsRunning] = useState(false)
-
-  const startStopMetronome = () => {
-    if (!metronome) {
-      const newMetronome = new Metronome
-      newMetronome.startStop()
-      setMetronome(newMetronome)
-    } else {
-      metronome.startStop()
-    }
-    setIsRunning(!isRunning)
-  }
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to Practice Buddy</h1>
-      <Button 
-        onClick={() => {
-          startStopMetronome()
-        }}
-      >
-        { isRunning 
-            ? 
-          <Icon 
-            path={'/images/pause.svg'} 
-            alt={'pause button'} 
-          />
-            :
-          <Icon 
-            path={'/images/play.svg'} 
-            alt={'play button'} 
-          />
-        }
-      </Button>
+      <MetronomeTool />
     </main>
   )
 }
