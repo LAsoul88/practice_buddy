a1c3f134c5e3357ddbcd44afbae511fd834845b5
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-17 17:20:44 -0700

    refined metronome logic to create after user initiated action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,30 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Button } from '@/app/(component)/Button'
 import { Icon } from '@/app/(component)/Icon'
 import Metronome from '@/util/metronome'
 
 export default function Home() {
-  const [metronome, setMetronome] = useState<Metronome>({} as Metronome)
+  const [metronome, setMetronome] = useState<Metronome>()
   const [isRunning, setIsRunning] = useState(false)
 
-  const startMetronome = () => {
-    if (metronome) {
+  const startStopMetronome = () => {
+    if (!metronome) {
+      const newMetronome = new Metronome
+      newMetronome.startStop()
+      setMetronome(newMetronome)
+    } else {
       metronome.startStop()
-      setIsRunning(!isRunning)
     }
-    console.log(metronome)
+    setIsRunning(!isRunning)
   }
-
-  useEffect(() => {
-    setMetronome(new Metronome)
-  }, [])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to Practice Buddy</h1>
       <Button 
         onClick={() => {
-          startMetronome()
+          startStopMetronome()
         }}
       >
         { isRunning 
