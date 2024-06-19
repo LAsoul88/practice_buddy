36c4f901ea78af7c942bda841c1e078c21d8f393
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-19 16:47:51 -0700

    added border property to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from 'react'
 interface ButtonProps {
   children: ReactNode
   type?: 'primary' | 'secondary'
+  border?: boolean
   disabled?: boolean
   onClick: () => void
 }
@@ -10,13 +11,14 @@ interface ButtonProps {
 export const Button = ({ 
   children, 
   type = 'primary', 
+  border,
   disabled = false, 
   onClick 
 }: ButtonProps) => {
   return (
     <button 
       disabled={disabled}
-      className={''}
+      className={`${border ? 'border border-2 border-oxfordBlue rounded-md' : ''} px-2 py-1`}
       onClick={onClick}
     >
       {children}
