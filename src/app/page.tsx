cf4da94c3c7bc500cd8e5af8d5d526d2f97a6108
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-20 14:03:05 -0700

    implemented Drone tool into main app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+import { DroneTool } from '@/components/DroneTool'
 import { MetronomeTool } from '@/components/MetronomeTool'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around bg-celadon text-oxford-blue">
       <h1 className="">Welcome to Practice Buddy</h1>
-      <section className="flex flex-col items-center w-1/2 max-w-[390px] min-w-[260px]">
-        <h2>Metronome</h2>
-        <MetronomeTool />
-      </section>
+      <div className="flex items-center text-center w-full">
+        <section className="flex flex-col items-center w-1/3">
+          <h2>Drone</h2>
+          <DroneTool />
+        </section>
+        <section className="flex flex-col items-center w-1/3 max-w-[390px] min-w-[260px]">
+          <h2>Metronome</h2>
+          <MetronomeTool />
+        </section>
+        <section className="flex flex-col items-center w-1/3">
+          <h2>Practice Journal</h2>
+        </section>
+      </div>
     </main>
   )
 }
