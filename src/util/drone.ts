f2053ee4746fa0f7240c09bb05c622175f78d9bc
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-20 15:58:15 -0700

    added envelope to Drone, created freqMap and typings for easy selection of commonly assigned note frequencies

--- src/util/drone.ts
+++ src/util/drone.ts
@@ -1,13 +1,33 @@
+interface FrequencyMap {
+  [key: string]: number
+}
+const freqMap: FrequencyMap = {
+  'C': 261.63,
+  'C#/Db': 277.18,
+  'D': 293.66,
+  'D#/Eb': 311.13,
+  'E': 329.63,
+  'F': 349.23,
+  'F#/Gb': 369.99,
+  'G': 392.00,
+  'G#/Ab': 415.30,
+  'A': 440.00,
+  'A#/Bb': 466.16,
+  'B': 493.88,
+}
+
 export class Drone {
   audioContext: AudioContext
   oscillator: OscillatorNode
+  envelope: GainNode
   pitch: number
   isRunning: boolean
 
-  constructor() {
+  constructor(note = 'A') {
     this.audioContext = new AudioContext()
     this.oscillator = this.audioContext.createOscillator()
-    this.pitch = 1000
+    this.envelope = this.audioContext.createGain()
+    this.pitch = freqMap[note]
     this.isRunning = false
   }
 
@@ -17,21 +37,30 @@ export class Drone {
     if (!this.audioContext) {
       this.audioContext = new AudioContext()
       this.oscillator = this.audioContext.createOscillator()
+      this.envelope = this.audioContext.createGain()
     }
-    this.oscillator.type = 'sine'
+    
+    this.envelope.gain.value = 1
+    // this.envelope.gain.exponentialRampToValueAtTime(1, 1)
+    
+    this.oscillator.type = 'triangle'
     this.oscillator.frequency.value = this.pitch
     this.oscillator.start(this.audioContext.currentTime)
-  }
 
+    this.oscillator.connect(this.envelope)
+  }
+  
   start() {
     console.log(this.oscillator.context)
     if (this.oscillator.context.state === 'suspended') this.activateOsc()
-    
+      
     this.oscillator.connect(this.audioContext.destination)
+    this.envelope.connect(this.audioContext.destination)
     this.isRunning = true
   }
   stop() {
     this.oscillator.disconnect(this.audioContext.destination)
+    this.envelope.disconnect(this.audioContext.destination)
     this.isRunning = false
   }
   startStop() {
