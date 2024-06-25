457ee05a5f4819c62aa40f7e15d9292dca5d38b8
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-25 16:03:08 -0700

    fixed spacing between elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-around bg-celadon text-oxford-blue">
       <h1 className="">Welcome to Practice Buddy</h1>
       <div className="flex items-center text-center w-full">
-        <section className="flex flex-col items-center w-1/3">
+        <section className="flex flex-col items-center w-1/3 min-h-[260px]">
           <h2>Drone</h2>
           <DroneTool />
         </section>
-        <section className="flex flex-col items-center w-1/3 max-w-[390px] min-w-[260px]">
+        <section className="flex flex-col items-center w-1/3 min-h-[260px]">
           <h2>Metronome</h2>
           <MetronomeTool />
         </section>
-        <section className="flex flex-col items-center w-1/3">
+        <section className="flex flex-col items-center w-1/3 min-h-[260px]">
           <h2>Practice Journal</h2>
         </section>
       </div>
