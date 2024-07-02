4515eb621cdc30d45f17bee2aeefbd0ff360385b
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-01 18:33:08 -0700

    built basic journal component and all tests passed

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+'use client'
+import { useState, FormEvent, ChangeEvent } from 'react'
+import { Button } from '@/components/Button'
 
 export default function Journal() {
+  const [entry, setEntry] = useState('')
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    setEntry('')
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const value: string = e.target.value
+    setEntry(value)
+  }
   return (
-    <div>
-      Journal
+    <div className="w-full h-full flex flex-col">
+      <h2>Practice Journal</h2>
+      <form className="flex flex-col w-fit h-fit" onSubmit={handleSubmit}>
+        <textarea 
+          className="input" 
+          value={entry}
+          onChange={handleChange}
+        ></textarea>
+        <Button
+          onClick={() => {}}
+        >
+          Submit
+        </Button>
+      </form>
     </div>
   )
 }
