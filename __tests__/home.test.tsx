0fa2a373cb75fda6e9bba0f7b03583bec480259b
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-01 17:46:34 -0700

    set up initial journal test suite

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,10 +1,10 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import Page from '@/app/page'
+import Home from '@/app/page'
 
 describe('Page', () => {
   it('renders a heading', () => {
-    render(<Page />)
+    render(<Home />)
 
     const sections = screen.getAllByRole('region')
     for (const section of sections) expect(section).toBeInTheDocument()
