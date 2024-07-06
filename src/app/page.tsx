ef758608d42c6943533fbd806d348e4ba086401c
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-06 14:03:27 -0700

    added styling for mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around">
       <h1 className="">Welcome to Practice Buddy</h1>
-      <div className="flex items-center text-center w-full">
-        <section className="flex flex-col items-center w-1/3 min-h-[260px]" role="region" aria-label="Drone">
+      <div className="flex flex-col md:flex-row items-center text-center w-full">
+        <section className="flex flex-col items-center w-full min-w-[350px] min-h-[260px]" role="region" aria-label="Drone">
           <h2>Drone</h2>
           <DroneTool />
         </section>
-        <section className="flex flex-col items-center w-1/3 min-h-[260px]" role="region" aria-label="Metronome">
+        <section className="flex flex-col items-center w-full min-w-[350px] min-h-[260px]" role="region" aria-label="Metronome">
           <h2>Metronome</h2>
           <MetronomeTool />
         </section>
-        <section className="flex flex-col items-center w-1/3 min-h-[260px]" role="region" aria-label="Practice Journal">
+        {/* <section className="flex flex-col items-center w-1/3 min-h-[260px]" role="region" aria-label="Practice Journal">
           <h2>Practice Journal</h2>
-        </section>
+        </section> */}
       </div>
     </main>
   )
