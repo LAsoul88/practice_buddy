d50f2a76c7b80c2807c0d653c5f6c89989f92732
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-16 12:40:08 -0700

    fixing copy typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around">
       <h1 className="">Welcome to Practice Buddy!</h1>
-      <p className="text-center text-lg my-12 md:mx-36">This application is developed with musicians in mind. I want to provide a suite of tools to promote a rapid, structured approach to learning learning. <br/>So far, we have a drone and metronome below to use. Stay tuned as we will be adding a practice journal, tuner, and other features in the coming weeks!</p>
+      <p className="text-center text-lg my-12 md:mx-36">This application is developed with musicians in mind. I want to provide a suite of tools to promote a rapid, structured approach to learning. <br/>So far, we have a drone and metronome below to use. Stay tuned as we will be adding a practice journal, tuner, and other features in the coming weeks!</p>
       <div className="flex flex-col md:flex-row items-center text-center w-full">
         <section className="flex flex-col items-center w-full min-w-[350px] min-h-[260px]" role="region" aria-label="Drone">
           <h2>Drone</h2>
