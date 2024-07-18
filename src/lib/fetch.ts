b8b103866d6d209b064f6f0f064c45453b69eff0
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-18 11:24:48 -0700

    added try/catch to both api helpers

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -3,23 +3,31 @@ import 'server-only'
 const baseURL = process.env.API_URL
 
 export const GET = async (url: string) => {
-  const result = await fetch(baseURL + url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    cache: 'no-store'
-  }).then(res => res.json())
-  return result
+  try {
+    const result = await fetch(baseURL + url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      cache: 'no-store'
+    }).then(res => res.json())
+    return result
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export const POST = async (url: string, body: any) => {
-  const result = await fetch(baseURL + url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ ...body })
-  }).then(res => res.json())
-  return result
+  try {
+    const result = await fetch(baseURL + url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ...body })
+    }).then(res => res.json())
+    return result
+  } catch (error) {
+    console.log(error)
+  }
 }
