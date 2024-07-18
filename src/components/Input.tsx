0a28b72ef95e9d8eacd92c7758a4ca7e55c22bc4
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-18 15:57:39 -0700

    reformatting Input to include react-hook-forms

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,85 +1,159 @@
 import { ChangeEvent } from 'react'
+import { UseFormRegister, FieldValues, Path } from 'react-hook-form'
 
-type InputProps = {
-  inputType?: 'input' | 'select' | 'textarea'
-  type?: string
-  id: string
-  name: string
-  min?: number
-  max?: number
-  value: number | string
+// type InputProps = {
+//   inputType?: 'input' | 'select' | 'textarea'
+//   type?: string
+//   id: string
+//   name: string
+//   min?: number
+//   max?: number
+//   value: number | string
+//   width?: string
+//   height?: string
+//   options?: string[]
+//   disabled?: boolean
+//   register?: UseFormRegister<FormRegisterValues>
+//   handleChange?: (e: ChangeEvent<FormElements>) => void
+// }
+
+// export const Input = ({ 
+//   inputType = 'input',
+//   type = 'number',
+//   id,
+//   name,
+//   min,
+//   max,
+//   value,
+//   width = '72px',
+//   height = '34px',
+//   options = [],
+//   disabled = false,
+//   register,
+//   handleChange
+// }: InputProps) => {
+//   const style = `input w-[${width}] h-[${height}]`
+//   const createInput = () => {
+//     switch (inputType) {
+//       case 'select':
+//         return (
+//           <select
+//             id={id}
+//             name={name}
+//             defaultValue={value}
+//             onChange={handleChange}
+//             className={style}
+//             style={{width: `${width}`, height: `${height}`}}
+//             disabled={disabled}
+//           >
+//             { options.map(option => {
+//               return <option key={option}>{option}</option>
+//             })}
+//           </select>
+//         )
+//       case 'textarea':
+//         return (
+//           <textarea
+//             id={id}
+//             name={name}
+//             value={value}
+//             onChange={handleChange}
+//             className={style}
+//             disabled={disabled}
+//           />
+//             // find a way to correctly handle change of text area
+//         )
+//       default:
+//         // if (register) return (
+//         //   <input 
+//         //     {...register(id)}
+//         //     id={id}
+//         //     name={name}
+//         //     min={min}
+//         //     max={max}
+//         //     className={style}
+//         //   />
+//         // )
+//         return (
+//           <input 
+//             type={type}
+//             name={name}
+//             id={id}
+//             min={min}
+//             max={max}
+//             value={value}
+//             onChange={handleChange}
+//             className={style}
+//             disabled={disabled}
+//           />
+//         )
+//     }
+//   }
+//   return (
+//     <>
+//       <label htmlFor={name}>{name}</label>
+//       { createInput() }
+//     </>
+//   )
+// }
+interface InputProps {
+  name: Path<RegistrationFormValues>
+  label: string
+  register: UseFormRegister<FormRegisterValues>
+  required?: boolean
   width?: string
   height?: string
-  options?: string[]
-  disabled?: boolean
-  handleChange: (e: ChangeEvent<FormElements>) => void
 }
 
 export const Input = ({ 
-  inputType = 'input',
-  type = 'number',
-  id,
   name,
-  min,
-  max,
-  value,
-  width = '72px',
-  height = '34px',
-  options = [],
-  disabled = false,
-  handleChange
+  label, 
+  register, 
+  required, 
+  width = '72px', 
+  height = '36px' 
 }: InputProps) => {
   const style = `input w-[${width}] h-[${height}]`
-  const createInput = () => {
-    switch (inputType) {
-      case 'select':
-        return (
-          <select
-            id={id}
-            name={name}
-            defaultValue={value}
-            onChange={handleChange}
-            className={style}
-            style={{width: `${width}`, height: `${height}`}}
-            disabled={disabled}
-          >
-            { options.map(option => {
-              return <option key={option}>{option}</option>
-            })}
-          </select>
-        )
-      case 'textarea':
-        return (
-          <textarea
-            id={id}
-            name={name}
-            value={value}
-            onChange={handleChange}
-            className={style}
-            disabled={disabled}
-          />
-            // find a way to correctly handle change of text area
-        )
-      default:
-        return (
-          <input 
-            type={type}
-            name={name}
-            id={id}
-            min={min}
-            max={max}
-            value={value}
-            onChange={handleChange}
-            className={style}
-            disabled={disabled}
-          />
-        )
-    }
-  }
   return (
     <>
-      <label htmlFor={name}>{name}</label>
-      { createInput() }
+      <label>{label}</label>
+      <input 
+        {...register(name, { required })}
+        className={style}
+      />
+    </>
+  )
+}
+
+interface SelectProps {
+  name: Path<RegistrationFormValues>
+  label: string
+  register: UseFormRegister<FormRegisterValues>
+  options: string[]
+  width: string
+  height: string
+}
+
+export const Select = ({ 
+  name,
+  label, 
+  register, 
+  options,
+  width = '72px', 
+  height = '36px'  
+}: SelectProps) => {
+  const style = `input w-[${width}] h-[${height}]`
+  return (
+    <>
+      <label>{label}</label>
+      <select
+        {...register(name)}
+        className={style}
+      >
+        { options.map(option => {
+          return <option key={option}>{option}</option>
+        })}
+      </select>
     </>
   )
 }
