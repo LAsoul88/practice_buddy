70308c8ff1a935b7f987ec63d926cb570dbee9b3
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-08-08 17:10:37 -0700

    separated POST into actions, added caching back into GET requests

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,30 +1,14 @@
-import 'server-only'
+'use server'
 
-const baseURL = process.env.API_URL
+const baseUrl = process.env.API_URL
 
 export const GET = async (url: string) => {
   try {
-    const result = await fetch(baseURL + url, {
+    const result = await fetch(baseUrl + url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
-      },
-      cache: 'no-store'
-    }).then(res => res.json())
-    return result
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-export const POST = async (url: string, body: any) => {
-  try {
-    const result = await fetch(baseURL + url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ ...body })
+      }
     }).then(res => res.json())
     return result
   } catch (error) {
