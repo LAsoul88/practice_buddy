db8d259710c54da8b2e21b512e43285ddd23c798
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-02 18:58:16 -0700

    implementing wad to attempt tuner build

--- src/util/tuner.ts
+++ src/util/tuner.ts
@@ -1,3 +1,10 @@
+import Wad from 'web-audio-daw'
+
+const voice = new Wad({ source: 'mic' })
+const tuner = new Wad.Poly({})
+tuner.setVolume(0)
+tuner.add(voice)
+
 export class Tuner {
   audioContext: AudioContext
   constructor() {
