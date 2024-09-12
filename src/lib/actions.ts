8aa8f777808371c5c1e5705d8f9bb3aafa5b375c
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-12 15:57:30 -0700

    formalized action structure and fixed cookie logic

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -21,32 +21,34 @@ const cookieOptions: {
   domain: 'localhost'
 }
 
-const getCookie = async (name: string) => {
+const getCookie = (name: string) => {
   return cookies().get(name)?.value ?? ''
 }
 
-export async function addEntry(formData: FormData) {
-  try {
-    const result = await fetch(baseUrl + '/entries', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ 
-        text: formData.get('Text'),
-        userId: formData.get('Id') 
-      }),
-      credentials: 'include'
-    }).then(res => res.json())
+const setCookie = async (name: string, token: string) => {
+  cookies().set(name, token, cookieOptions)
+}
 
-    revalidatePath(`/journal/${result.userId}`)
-  } catch (error) {
-    console.log(error)
-  }
+export async function addEntry(formData: FormData) {
+  const res = await fetch(baseUrl + '/entries', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 
+      text: formData.get('Text'),
+      userId: formData.get('Id') 
+    }),
+    credentials: 'include'
+  })
+  
+  const data = await res.json()
+
+  revalidatePath(`/journal/${data.userId}`)
 }
 
 export async function login(formData: FormData) {
-  const response = await fetch(baseUrl + '/auth/login', {
+  const res = await fetch(baseUrl + '/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -58,55 +60,55 @@ export async function login(formData: FormData) {
     credentials: 'include'
   })
 
-  const setCookie = response.headers.getSetCookie()
-  const accessToken = setCookie.find(cookie => cookie.includes('accessToken'))?.split('=')[1].split(';')[0]
-  // const refreshToken = setCookie.find(cookie => cookie.includes('refreshToken'))?.split('=')[1]
-  // const userSession = setCookie.find(cookie => cookie.includes('userSession'))?.split('=')[1]
-  console.log('accessToken', accessToken)
-  
-  if (accessToken) cookies().set('accessToken', accessToken, cookieOptions)
-  // if (refreshToken) cookies().set('refreshToken', refreshToken, cookieOptions)
-  // if (userSession) cookies().set('userSession', userSession, cookieOptions)
-  
-  const data = await response.json() 
+  const data = await res.json() 
   if (data.error) return console.log(data.error)
-  // return data
-  revalidatePath(`/journal/${data.user._id}`)
-  redirect(`/journal/${data.user._id}`)
+  const { user, accessToken, refreshToken } = data
+  
+  if (accessToken) setCookie('accessToken', accessToken)
+  if (refreshToken) setCookie('refreshToken', refreshToken)
+  
+  redirect(`/journal/${user._id}`)
 }
 
 export async function register(formData: FormData) {
-  try {
-    const result = await fetch(baseUrl + '/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: formData.get('email'),
-        username: formData.get('username'),
-        password: formData.get('password')
-      }),
-      credentials: 'include'
-    })
-    console.log('that', result)
-  } catch (error) {
-    console.log(error)
-  }
+  const res = await fetch(baseUrl + '/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: formData.get('email'),
+      username: formData.get('username'),
+      password: formData.get('password')
+    }),
+    credentials: 'include'
+  })
+  
+  const data = await res.json()
+  if (data.error) return console.log(data.error)
+  
+  const { user, accessToken, refreshToken } = data
+  if (accessToken) setCookie('accessToken', accessToken)
+  if (refreshToken) setCookie('refreshToken', refreshToken)
+
+  redirect(`/journal/${user._id}`)
 }
 
 export const getEntries = async (url: string) => {
-  const cookie = await getCookie('accessToken')
-  const result = await fetch(baseUrl + url, {
+  const accessToken = 'accessToken=' + getCookie('accessToken')
+  const refreshToken = 'refreshToken=' + getCookie('refreshToken')
+  if (!accessToken && !refreshToken) return 'No valid token present'
+
+  const res = await fetch(baseUrl + url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Credentials': 'true',
-      Cookie: 'accessToken=' + cookie
+      Cookie: accessToken ? accessToken + ';' + refreshToken : refreshToken
     },
     credentials: 'include'
-  }).then(res => res.json())
+  })
 
-  if (result.error) return result.error
-  return result
+  const data = await res.json()
+  if (data.error) return data.error
+  return data
 }
