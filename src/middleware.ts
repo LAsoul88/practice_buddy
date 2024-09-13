aa93115d3ef00d4be72bcdf64163a95fecb93917
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-12 17:06:30 -0700

    removed cookie logger in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const cookies = request.headers.get('Cookie')
-  console.log(cookies)
   return NextResponse.next()
 }
