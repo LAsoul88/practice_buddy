32a169b836ab1f5637eb4f1fc51b125bdcb21692
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-16 21:22:04 -0700

    set up header with session info

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,16 +4,28 @@ import { cookies } from 'next/headers'
 export const Header = async () => {
   const userCookie = cookies().get('userSession')?.value
   let username = ''
-  if (userCookie) username = JSON.parse(userCookie).username
-  console.log(userCookie)
+  let _id = ''
+  if (userCookie) {
+    username = JSON.parse(userCookie).username
+    _id = JSON.parse(userCookie)._id
+  }
   return (
     <header className="sticky top-0 z-50 w-full h-[48px] bg-oxfordBlue text-platinum">
       <nav className="w-full h-full flex">
         <ul className="w-full h-full flex justify-end items-center gap-4 p-4">
-          <li><Link href="/register">Register</Link></li>
-          { userCookie ? <li>{username}</li> : null}
-          
-          <li><Link href="/login">Login</Link></li>
+          { userCookie 
+              ? 
+            <>
+              <li><Link href={`/journal/${_id}`}>{username}</Link></li> 
+              <li><Link href="">Logout</Link></li> 
+            </>  
+              : 
+            <>
+              <li><Link href="/register">Register</Link></li>
+              <li><Link href="/login">Login</Link></li>
+            </>
+          }
+            
 
         </ul>
       </nav>
